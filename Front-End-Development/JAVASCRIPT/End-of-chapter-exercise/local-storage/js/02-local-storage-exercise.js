752e279e3a1a3f156a92cffc46674cb189f42332
Onat <82000752+main-hesflay@users.noreply.github.com>
2022-02-03 04:02:52 -0800

    Update 02-local-storage-exercise.js

--- Front-End-Development/JAVASCRIPT/End-of-chapter-exercise/local-storage/js/02-local-storage-exercise.js
+++ Front-End-Development/JAVASCRIPT/End-of-chapter-exercise/local-storage/js/02-local-storage-exercise.js
@@ -5,7 +5,7 @@ let counterDOM = document.querySelector("#counter") // Number
 let increaseDOM = document.querySelector("#increase") // Buttons
 let decreaseDOM = document.querySelector("#decrease") 
 
-counterDOM.innerHTML = counter // varsayilan sayi 0
+counterDOM.innerHTML = counter // varsayilan sayi 0 olarak ekranda gözükücek
 
 increaseDOM.addEventListener("click", clickEvent) // Tiklandiginda 
 decreaseDOM.addEventListener("click", clickEvent)
@@ -13,9 +13,9 @@ decreaseDOM.addEventListener("click", clickEvent)
 
 function clickEvent() {
     console.log(this.id);
-    this.id == "increase" ? counter += 1 : counter -= 1
+    this.id == "increase" ? counter += 1 : counter -= 1 // eger buton "increase" esitse yani bastiysa arttir degilse eksilt buda Eksilt Butonu oluyor
 
     localStorage.setItem('counter', counter) // counter adinda Local Storage olusturduk ve icerisine counter bilgisini atadik
 
-    counterDOM.innerHTML = counter
+    counterDOM.innerHTML = counter // her bastiginda ekrana sayiyi yazdirir.
 }
