6546286c6d0952fe471632ff2af47a9d968aed8a
Onat Dibo <82000752+main-hesflay@users.noreply.github.com>
2022-03-30 01:12:49 -0700

    Update main.js

--- Front-End-Development/JAVASCRIPT/Task-1/js/main.js
+++ Front-End-Development/JAVASCRIPT/Task-1/js/main.js
@@ -1,39 +1,52 @@
-let userName = prompt("Adinizi giriniz");
-let info = document.querySelector("#name");
+let userName = prompt("Adınızı Girin")
+let info = document.querySelector('#name');
+let failed = document.querySelector('#failedMessage')
 
-info.innerHTML = `Merhaba ${userName}! Hoşgeldin`
+
+if (!userName) {
+    alert("Kullanıcı Adı Boş Girilemez!")
+    document.querySelector('#clock').remove();
+    failed.innerHTML = "Hata ile Karşılaşıldı!"
+    console.warn("Kullanıcı Adı Giriniz!");
+} else {
+    info.innerHTML = `Merhaba ${userName} Hoşgeldin`
+}
+
 
 function clock() {
-
     const today = new Date();
     let h = today.getHours();
     let m = today.getMinutes();
     let s = today.getSeconds();
     let d = today.getDay();
-    m = checkTime(m);
-    s = checkTime(s);
+    m = checktime(m);
+    s = checktime(s);
 
-    if ( d == 1) {
-        d = "Pazartesi";
-    } else if ( d == 2) {
-        d = "Salı";
-    } else if ( d == 3) {
-        d = "Çarşamba";
-    } else if ( d == 4) {
-        d = "Perşembe";
-    } else if ( d == 5) {
-        d = "Cuma";
-    } else if ( d == 6) {
-        d = "Cumartesi";
-    } else if ( d == 0) {
-        d = "Pazar";
+    if (d == 1) {
+        d = "Pazartesi"
+    } else if (d == 2) {
+        d = "Salı"
+    } else if (d == 3) {
+        d = "Çarşamba"
+    } else if (d == 4) {
+        d = "Perşembe"
+    } else if (d == 5) {
+        d = "Cuma"
+    } else if (d == 6) {
+        d = "Cumartesi"
+    } else if (d == 0) {
+        d = "Pazar"
     }
 
-    document.querySelector("#clock").innerHTML = h + ":" + m + ":" + s + " "+ d;
+    document.querySelector("#clock").innerHTML = h + ":" + m + ":" + s + " " + d;
     setTimeout(clock, 1000);
+
 }
 
-function checkTime(i) {
-    if (i < 10) {i = "0" + i};
+function checktime(i) {
+    if (i < 10) { i = "0" + i };
     return i;
+
 }
+
+// Boşluk atınca uygulama çalışıyor o sorunu halledicem
