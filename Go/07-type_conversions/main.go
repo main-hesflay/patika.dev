3d7f41c85a8e9c02f65610780141e7c75d376945
Hesflay <hesflay@outlook.com>
2022-09-07 15:08:18 +0300

    Create main.go

---
+++ Go/07-type_conversions/main.go
@@ -0,0 +1,53 @@
+package main
+
+import "fmt"
+
+func main() {
+
+	/* x := 10
+	y := 10.0
+
+	fmt.Printf("%v %T\n", x, x)
+	fmt.Printf("%v %T\n", y, y)
+
+	// Type Conversion type(y) => int(y) = 10.0 => 10
+
+	fmt.Println(x + int(y))
+
+	fmt.Printf("%v %T\n", y, y) */
+
+	/*	var x int8 = 10
+		var y int16 = 10
+
+		fmt.Println(x + y) */
+
+	/* x := 10
+	y := 10.0
+
+	fmt.Printf("%v %T\n", x, x)
+	fmt.Printf("%v %T\n", y, y)
+
+	fmt.Println(float64(x) + y) */
+
+	/*	var x int8 = 127 // type value
+		var y int16
+
+		y = int16(x)
+
+		fmt.Println(y) */
+
+	/* x := 10
+	y := "10"
+
+	fmt.Printf("%v %T\n", x, x)
+	fmt.Printf("%v %T\n", y, y)
+
+	fmt.Println(x + int(y)) */
+
+	num1 := 106
+	str1 := string(num1)
+
+	fmt.Printf("%v %T\n", num1, num1)
+	fmt.Printf("%v %T\n", str1, str1)
+
+}
