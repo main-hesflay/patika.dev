4d8265b8928bec77616cf61639e149ce42521ec5
Hesflay <hesflay@outlook.com>
2022-09-14 06:11:49 +0300

    Create main.go

---
+++ Go/08_print_println_printf/main.go
@@ -0,0 +1,37 @@
+package main
+
+func main() {
+
+	/*	fmt.Println("Hello")
+		fmt.Print("Hello")
+		fmt.Println("")
+		fmt.Printf("Hello") */
+
+	// name := "Onat"
+
+	/*	fmt.Print(name)
+		fmt.Println(name)
+		fmt.Print(name) */
+
+	/*	fmt.Print("My name's", name)
+		fmt.Println("")
+		fmt.Println("My name's", name)
+		fmt.Printf("My name's %X %T", name, name) */
+
+	/*	x := 100
+		y := 20
+		z := 30
+
+		fmt.Printf("%b %d %o", x, y, z) */
+
+	// name, age := "Onat", 20
+
+	// fmt.Print("My name's ", name, " and I'm ", age, " years old.")
+
+	// fmt.Println("My name's", name, "and I'm", age, "years old.")
+
+	// fmt.Printf("My name's %v and I'm %v years old.", name, age)
+
+}
+
+// it's not over :)
