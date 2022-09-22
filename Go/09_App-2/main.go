f2e26cfe000f959c39220d3af6b684a4767a510e
Hesflay <hesflay@outlook.com>
2022-09-22 09:57:46 +0300

    Update main.go

--- Go/09_App-2/main.go
+++ Go/09_App-2/main.go
@@ -1,4 +1,4 @@
-// 1 - int x, float64 y type conversion sample
+// 1 -) int x, float64 y type conversion sample
 
 /* package main
 
@@ -14,27 +14,83 @@ func main() {
 
 } */
 
-// 2 - multiple assing sample x, y = y, x
+// 2 -) multiple assing sample x, y = y, x
+
+// package main
+
+// func main() {
+
+// 		x := 5
+// 		y := 10
+
+// 		fmt.Println("Before |", "X:", x, "Y:", y)
+
+// 		x, y = y, x // x = y, y = x
+
+// 		fmt.Println("After |", "X:", x, "Y:", y)
+
+// }
+
+// 3 -) non English variable names
+
+// package main
+
+// import "fmt"
+
+// func main() {
+
+// 	/*	yaş := 21
+
+// 		fmt.Println(yaş) */
+
+// 	姓名 := "Onat"
+// 	年龄 := 21
+
+// 	fmt.Println("Name:", 姓名, "Age:", 年龄)
+
+// }
+
+// 4 -) shadowing ? - gölgeleme
+
+// package main
+
+// import "fmt"
+
+// func main() {
+
+// 	x := 5
+
+// 	if true {
+
+// 		x = 10
+// 		x++
+
+// 		fmt.Println(x)
+// 	}
+
+// 	fmt.Println(x)
+// }
+
+// 5 -) 40 as a string
 
 package main
 
-import "fmt"
+import (
+	"fmt"
+	"strconv"
+)
 
 func main() {
 
-	x := 5
-	y := 10
+	x := 40
 
-	fmt.Println("Before |", "X:", x, "Y:", y)
+	s := string(x)
 
-	x, y = y, x
+	fmt.Printf("%v %T\n", x, x) // 40
+	fmt.Printf("%v %T\n", s, s) // (
 
-	fmt.Println("After |", "X:", x, "Y:", y)
+	y := strconv.Itoa(x) // strconv.Itoa = int to string
 
-} // 7:20
+	fmt.Printf("%v %T\n", y, y) // "65"
 
-// 3 - non English variable names
-
-// 4 - shadowing
-
-// 5 - 40 as a string
+}
